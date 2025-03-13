82f5df5b6d9341773e7f4f5c77b93d313294b8e4
Li Yansong <lyuanshe@gmail.com>
2025-03-13 12:41:16 +0000

    some change on old version
    adapt thread to make gyro sensor read and show data

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
-#include <thread>
-#include<unistd.h>
-#include"PID.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <cmath>
+#include<gpiod.h>
+#include<mutex>
+#include"mpu6050_test001.h"
+
+// protect data
+std::mutex mtx;
+
 int main()
-{   while(true)
-    {
-        sleep(1);
-    }
+{   
+    MPU6050 MPU;
+    
+    //RUN thread
+    std::thread sensor_thread(&MPU6050::run, &MPU);
+    // wait the thread finish
+    sensor_thread.join();
     return 0;
 }
