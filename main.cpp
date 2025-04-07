c58a83a17420feb0626520278798e9799b9abc4c
Li Yansong <lyuanshe@gmail.com>
2025-04-07 15:28:52 +0100

    add motor driver
    add DRV8825

--- main.cpp
+++ main.cpp
@@ -8,23 +8,39 @@
 #include<mutex>
 #include"mpu6050_test001.h"
 #include"PID.h"
+#include "DRV8825.hpp"
 
 // protect data
 std::mutex mtx;
 
 int main()
 {   
+    // initialize MPU6050  DRV8825
     MPU6050 MPU;
     
-    // Register the callback interface: when new data is available, the callback is invoked
+    DRV8825::SelectMotor(DRV8825::MOTOR1);
+    DRV8825::Enable();
+
+    DRV8825::SelectMotor(DRV8825::MOTOR2);
+    DRV8825::Enable();
+
+    //RUN thread using call back
     MPU.setCallback([](float pitch, float ax) {
-        int pid_output = Vertical(0, pitch, ax);
-        std::cout << "PID output: " << pid_output << std::endl;
+        std::lock_guard<std::mutex> lock(mtx);
+
+        float target_velocity = Vertical(0,pitch, ax);
+    // Using PID
+        float temp_left = Velocity(target_velocity);
+        float temp_right = Velocity(target_velocity);
+
+        DRV8825::SelectMotor(DRV8825::MOTOR1);
+        driveMotorWithVelocityOutput(temp_left);
+        DRV8825::SelectMotor(DRV8825::MOTOR2);
+        driveMotorWithVelocityOutput(temp_right);
     });
-
-    // Run the sensor data reading thread
+    
     std::thread sensor_thread(&MPU6050::run, &MPU);
+    // wait the thread finish
     sensor_thread.join();
-
     return 0;
 }
