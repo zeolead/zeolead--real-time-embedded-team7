91ebcfce3aa52071037430ce2a02dd6bf872232a
Li Yansong <lyuanshe@gmail.com>
2025-04-07 15:30:06 +0100

    change headfile
    headfile for new PID.cpp

--- PID.h
+++ PID.h
@@ -1,11 +1,24 @@
 #ifndef PID_H
 #define PID_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-int Vertical(float Expect, float Angle, float gyro_x);
-float Velocity(float Expect_velocity, float velocityL, float velocityR);
+// PID  controller
+float Vertical(float Expect, float Angle, float gyro_x);
+float Velocity(float angle_pid_output);
 
+// Motor control
+void driveMotorWithVelocityOutput(float motor_speed_output);
 
-// int Turn(float gyro_z, int Target);
+// PID Coefficent
+extern float vertical_kp, vertical_ki, vertical_kd;
+extern float velocity_kp, velocity_ki, velocity_kd;
+extern float turn_kp, turn_ki, turn_kd;
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
